28aca2a8be2ca32571a6af17f25f80ff2a88f90d
Douglas <douglasd3v.contato@gmail.com>
2026-06-24 16:08:39 -0300

    feat: add category model and migration

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const Category = sequelize.define(
+  "Category",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  { tableName: "categories" },
+);
+
+export default Category;
--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,8 @@
 import User from "./User.js";
 import Role from "./Role.js";
 
+import Category from "./Category.js";
+
 Role.hasMany(User, {
   foreignKey: "roleId",
   as: "users",
